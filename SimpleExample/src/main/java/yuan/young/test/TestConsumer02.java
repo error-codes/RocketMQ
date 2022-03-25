f526130c722b78d7fda81bb61c76e8ca33e91aa3
BayMax <2309590834@qq.com>
2022-03-25 19:09:45 +0800

    测试理念

---
+++ SimpleExample/src/main/java/yuan/young/test/TestConsumer02.java
@@ -0,0 +1,38 @@
+package yuan.young.test;
+
+import org.apache.rocketmq.client.consumer.DefaultLitePullConsumer;
+import org.apache.rocketmq.client.consumer.DefaultMQPullConsumer;
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.MessageSelector;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.producer.MessageQueueSelector;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.common.message.MessageQueue;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author error-codes【BayMax】
+ * @see <a href="www.error-codes.xyz">BayMax Blog</a>
+ * @since 2022/3/25 11:12
+ */
+public class TestConsumer02 {
+
+    public static void main(String[] args) throws Exception {
+        DefaultLitePullConsumer consumerGroup01 = new DefaultLitePullConsumer("TestConsumerGroup01");
+
+        consumerGroup01.setNamesrvAddr("124.221.180.113:9876");
+        consumerGroup01.start();
+
+        MessageQueue partition = new MessageQueue("Topic", "broker-a", 0);
+
+        List<MessageExt> messageExts = consumerGroup01.poll();
+        for (MessageExt messageExt : messageExts) {
+            System.out.println("接收到消息：[QueueID:" + messageExt.getQueueId() + ", " + new String(messageExt.getBody()) + "]");
+        }
+
+    }
+}
